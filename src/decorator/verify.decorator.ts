74317ed977f36976f5971d6dd9e597b14458c961
vudinhduong <duong2lophot@gmail.com>
2024-07-04 23:43:48 +0700

    test

--- src/decorator/verify.decorator.ts
+++ src/decorator/verify.decorator.ts
@@ -12,8 +12,8 @@ import { Request } from 'express';
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private jwtService: JwtService, private configService: ConfigService) { }
-
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const a=1
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
         if (!token) {
